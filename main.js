295f714b391638a15a9b4c2396b45b89307f376a
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-04-26 14:50:29 -0400

    complete

--- main.js
+++ main.js
@@ -3,8 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.querySelector("#modal");
+errorModal.classList.add("hidden");
 
-
+document.addEventListener('DOMContentLoaded', () => {
+  const likeGlyphs = document.querySelectorAll('.like-glyph');
+  for (let glyph of likeGlyphs) {
+    glyph.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          if (glyph.innerHTML === EMPTY_HEART) {
+            glyph.innerHTML = FULL_HEART;
+            glyph.classList.add('activated-heart');
+          } else {
+            glyph.innerHTML = EMPTY_HEART;
+            glyph.classList.remove('activated-heart');
+          }
+        })
+        .catch(() => {
+          errorModal.classList.remove('hidden');
+          const errorMessage = document.querySelector('#modal-message');
+          errorMessage.innerText = "Server error. Please try again.";
+          setTimeout(() => {
+            errorModal.classList.add('hidden');
+          }, 1000);
+        });
+    });
+  }
+});
 
 
 //------------------------------------------------------------------------------
